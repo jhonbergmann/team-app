8001a7399e307e518b90de09a973f8e5a692be94
Jhonatan Bergmann <54135650+jhonbergmann@users.noreply.github.com>
2024-07-16 22:53:55 -0300

    app

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import {StatusBar} from 'react-native'
+import {ThemeProvider} from 'styled-components/native'
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+
+import theme from '@/theme'
+import {Routes} from '@/routes'
+import {Loading} from '@components/Loading'
+
+export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </>
+    </ThemeProvider>
+  )
+}
